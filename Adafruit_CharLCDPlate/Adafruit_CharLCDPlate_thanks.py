2c0e1190a99b941fcda642f2157927d9f6e877f1
Casey Grzecka <c@sey.gr>
2014-01-13 08:06:36 +0000

    makes text scroll to the left then right once

--- Adafruit_CharLCDPlate/Adafruit_CharLCDPlate_thanks.py
+++ Adafruit_CharLCDPlate/Adafruit_CharLCDPlate_thanks.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
+from time import sleep
 
 # Initialize LCD plate
 lcd = Adafruit_CharLCDPlate()
@@ -7,7 +8,24 @@ lcd = Adafruit_CharLCDPlate()
 # Clear display
 lcd.clear()
 
+# Put message in a variable
+message = "Thanks Grant & Bebe! Love Domnic & Trent"
+
 # Output "Thank You" message
-lcd.message("Thx Grant & Bebe \n" +
-            "Love Dom & Trent")
+lcd.message(message)
+# Calc how many spaces to move
+move = len(message) - 16
+
+sleep(1)
+
+# Scroll display
+for x in range(0,move):
+  lcd.scrollDisplayLeft()
+  sleep(0.15)
+# Wait half a second
+sleep(0.5)
+# Scroll display back
+for x in range(0, move):
+  lcd.scrollDisplayRight()
+  sleep(0.08)
 
